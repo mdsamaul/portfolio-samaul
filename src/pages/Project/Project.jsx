858e85cb4a24af3ccd38b7638bfc1f501f6d2feb
Md Samaul <mdsamaul843#gmail.com>
2023-08-09 12:58:14 +0600

    update and working

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,10 +1,14 @@
-
 const Project = () => {
-    return (
-        <div>
-            this is Project pages
-        </div>
-    );
+  return (
+    <div className="pt-20">
+      <div
+        className="delay-[300ms] duration-[600ms] taos:[transform:translate3d(200px,200px,0)] taos:opacity-0"
+        data-taos-offset="300"
+      >
+        this is Project pages
+      </div>
+    </div>
+  );
 };
 
 export default Project;
